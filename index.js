7b38a8dae9a3bb91f86d9587fe6e7fdadccb24f4
IvanitoGamer <ivanlausinperdices@gmail.com>
2024-10-10 15:58:44 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const yesBtn = document.querySelector('#yesBtn');
+
+yesBtn.addEventListener('click',function () {
+    alert('Siiii, te amo muchisimo mi niña :D')
+});
+
+const noBtn = document.querySelector('#noBtn');
+
+noBtn.addEventListener('click', function() {
+    alert('Ala joooo voy a llorar...')
+});
